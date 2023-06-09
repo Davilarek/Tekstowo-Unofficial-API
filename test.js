16ed2eda79620c79a36f0d340524fead664a64d5
Daniel <davilareko@gmail.com>
2023-06-09 13:33:18 +0200

    Add some info to tests

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 const TekstowoAPI = require("./index");
 const TekstowoAPIInstance = new TekstowoAPI(fetch, 0);
 (async () => {
-    const lyricsExtracted = await TekstowoAPIInstance.extractLyrics(`frontside,dopoki_moje_serce_bije`);
+    const lyricsExtracted = await TekstowoAPIInstance.extractLyrics(`frontside,dopoki_moje_serce_bije`); // ok
     console.log(lyricsExtracted);
-    console.log(await TekstowoAPIInstance.searchLyrics("FrontSide", "Dopóki serce bije"));
-    console.log(await TekstowoAPIInstance.searchLyrics("", "Afterlife"));
-    console.log(await TekstowoAPIInstance.getPagesForSong("", "Afterlife"));
-    console.log(await TekstowoAPIInstance.getPagesForSong("FrontSide", "Dopóki serce bije"));
-    console.log(await TekstowoAPIInstance.getLyrics("FrontSide", "Dopóki serce bije"));
-    console.log(await TekstowoAPIInstance.extractLyrics(`frontside,dopoki_moje_serce_bijea`));
-    console.log(await TekstowoAPIInstance.searchLyrics("", "1234566777"));
+    console.log(await TekstowoAPIInstance.searchLyrics("FrontSide", "Dopóki serce bije")); // ok
+    console.log(await TekstowoAPIInstance.searchLyrics("", "Afterlife")); // ok
+    console.log(await TekstowoAPIInstance.getPagesForSong("", "Afterlife")); // ok
+    console.log(await TekstowoAPIInstance.getPagesForSong("FrontSide", "Dopóki serce bije")); // ok
+    console.log(await TekstowoAPIInstance.getLyrics("FrontSide", "Dopóki serce bije")); // ok
+    console.log(await TekstowoAPIInstance.extractLyrics(`frontside,dopoki_moje_serce_bijea`)); // not ok on purpose; typo
+    console.log(await TekstowoAPIInstance.searchLyrics("", "1234566777")); // not ok on purpose; no lyrics with that title
 })();
